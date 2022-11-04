146237dcbd802867267fb5174428fd40e63b8c78
Nick <nikita.nikolenko@gmail.com>
2022-11-04 01:18:45 +0000

    Added Firestore for saving events on the cloud

--- components/NavUserProfile.tsx
+++ components/NavUserProfile.tsx
@@ -3,22 +3,18 @@ import styles from '../styles/Navbar.module.css'
 import { useStore } from '../utils/store';
 import { auth, signInWithGoogle } from '../utils/firebase-config';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
+import Image from 'next/image';
 
 const NavUserProfile = () => {
 	const [authState, setAuthState] = useStore(
 			(state : any) => [state.authDetails, state.setAuthDetails]
 	)
+  useEffect(() => {
+    console.log(authState)
+  }, [])
 	const signIn = () => {
-    signInWithGoogle().then((result) => {
-      // console.log(result.user.email)
-      // setAuthState({
-      //   name: result.user.displayName,
-      //   email: result.user.email,
-      //   photoURL: result.user.photoURL,
-      //   id: result.user.uid,
-      //   loggedIn: true
-      // })
-    }).catch((err) => console.log(err))
+    signInWithGoogle()
+    .catch((err) => console.log(err))
   }
 
 	if(!authState.loggedIn) return (
@@ -26,7 +22,7 @@ const NavUserProfile = () => {
 	)
   return (
     <div className={styles.account}>
-        <div className={styles.profile_wrapper}><img src={authState.photoURL} alt={authState.name} /></div>
+        <div className={styles.profile_wrapper}><Image layout='fill' src={authState.photoURL} alt={authState.name} /></div>
         <button className={styles.logout} onClick={() => signOut(auth)}>Logout</button>
     </div>
   )
